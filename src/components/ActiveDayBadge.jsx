ea42a0f5eb33aaa06a42666c906d6be7eca7f40e
henriktoth <henriktoth0517@gmail.com>
2025-07-10 22:58:47 +0200

    revert to phase1

--- src/components/ActiveDayBadge.jsx
+++
@@ -1,9 +0,0 @@
-function ActiveDayBadge(props){
-    return(
-        <div className="m-5 px-5 py-3 w-40 rounded-xl bg-gradient-to-r from-indigo-900 to-indigo-800">
-            <h2 className="text-center font-bold uppercase  tracking-wide">{props.activeDay}</h2>
-        </div>
-    )
-    
-}
-export default ActiveDayBadge
